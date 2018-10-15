1c802477bb9b9b2cdf5469426511c0bfe898b4c5
tesla <kela256@gmail.com>
2018-10-15 10:54:23 +0300

    made a component files

--- CarServiceFront/src/app/app.module.ts
+++ CarServiceFront/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FooterComponent } from './share/footer.component';
+import { HeaderComponent } from './share/header.component';
+import { AppointmentComponent } from './page/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AppointmentComponent,
   ],
   imports: [
     BrowserModule,
